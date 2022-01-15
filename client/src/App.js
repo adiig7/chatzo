9436b8464bc3eea5e8b6412f09df621f9f00b261
Aditya Gupta <adutyagupta765469@gmail.com>
2022-01-15 11:55:51 +0530

    added apikey

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
+const apiKey = "yhj9qt7zvveb";
+
+const client = StreamChat.getInstance(apiKey);
 function App() {
   return (
-    <div className="App">
+    <div className="app__wrapper">
+      <Chat client={client} theme='team light'>
+        <ChannelListContainer />
+        <ChannelListContainer />
+      </Chat>
     </div>
   );
 }
